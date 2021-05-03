e294dddd3319c5264fd853cf26fbfc39fe614ddf
thedrgman <80590783+thedrgman@users.noreply.github.com>
2021-05-03 18:04:26 -0400

    Update game.py

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -8,12 +8,12 @@ class Game:
         self.phrase = [
             Phrase("do not stop believing"),
             Phrase("trust in Jesus"),
-            Phrase("eyes to the window to the soul"),
+            Phrase("eyes are the window to the soul"),
             Phrase("united we stand"),
             Phrase("do not stop the music")
         ]
         self.active_phrase = None
-        self.guesses = []
+        self.guesses = [" ",]
         self.lives = 10
         
         
@@ -21,7 +21,7 @@ class Game:
         self.active_phrase = random.choice(self.phrase)
         
         
-    def welcome():
+    def welcome(self):
         print(""" Welcome to the Phase Hunter Game
         +++++++++++++++++++++++++++++++++++++++++++
         A random phrase is chosen and you 
@@ -35,7 +35,8 @@ class Game:
         
         
     def get_guess(self):
-        return input("Guess a letter.")
+        user_input = input("\nGuess a letter. ")
+        return user_input.lower()
     
     
     def game_over(self):
@@ -44,19 +45,24 @@ class Game:
         else:
             print("You ran out of guesses. The phrase was {}!".format(self.active_phrase))
         new_game = input("Would you like to play again? Type Y/N")
-        if lower(new_game) == 'y':
-            start()
+        if new_game.lower() == 'y':
+            self.guesses = [" ",]
+            self.start()
+        else:
+            print("Thank you for playing. See you next time!!!")
         
         
     def start(self):
         self.welcome()
         self.missed = 0
-        self.active_phrase = get_random_phrase()
-        while self.missed < self.lives or self.active_phrase.check_complete(self.guesses) == False:
+        self.get_random_phrase()
+        while self.missed < self.lives:
             display = self.active_phrase.display(self.guesses)
             user_guess = self.get_guess()
             error = True
             while error == True:
+                if user_guess == "quit":
+                    break
                 if user_guess.isalpha():
                     if len(user_guess) == 1:
                         break
@@ -73,11 +79,14 @@ class Game:
                 print("You already guessed that letter, pick another letter")
             else:
                 self.guesses.append(user_guess)
-                if user_guess in self.active_phrase:
+                if self.active_phrase.check_letter(user_guess):
+                    print("pass")
                     display
                 else:
                     print("You have guessed an incorrect letter. Try again.")
                     self.missed += 1
                     print("You have {} out of {} lives remaining!".format((self.lives - self.missed), self.lives))
             display
-        game_over()
+            if self.active_phrase.check_complete(self.guesses):
+                break
+        self.game_over()
